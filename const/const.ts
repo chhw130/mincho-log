50d4bcff7f280e9d6cac5a91991df98dc60375e1
chhw130 <527coco@naver.com>
2024-11-05 21:38:22 +0900

    feat : favicon

--- const/const.ts
+++ const/const.ts
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import path from 'path'
 
 export const META_DATA: Metadata = {
+  metadataBase: new URL('https://www.mincho130.xyz/'),
   title: 'Mincho-Log',
   description: 'Welcome to mincho-log',
   keywords: ['mincho', '527coco', 'chhw130', '민초', '기록'],
@@ -11,5 +12,10 @@ export const META_DATA: Metadata = {
     siteName: '민초로그',
     images: path.join(process.cwd(), '/images/mincho'),
     type: 'website',
+  },
+
+  icons: {
+    icon: '/images/mincho-rabbit.png',
+    apple: '/images/mincho-rabbit.png',
   },
 }
