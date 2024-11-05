b08f8a0983a952e36d2f06fb40050fcf0ffb0f68
chhw130 <527coco@naver.com>
2024-11-05 20:14:10 +0900

    test : parsing data log

--- app/(blog)/posts/[category]/[title]/page.tsx
+++ app/(blog)/posts/[category]/[title]/page.tsx
@@ -1,5 +1,9 @@
 import { Box, Center, Divider } from '@chakra-ui/react'
-
+import PostComment from '@/component/post/PostComment'
+import PostDetailHeader from '@/component/post/PostDetailHeader'
+import { Post } from '@/types/post'
+import { parsePostDetail } from '@/util'
+import readingTime from 'reading-time'
 interface PageParams {
   params: {
     category: string
@@ -9,20 +13,22 @@ interface PageParams {
 
 const page = async ({ params: { category, title } }: PageParams) => {
   console.log(category, title)
-  // const { data, content } = await parsePostDetail(category, title)
+  const { data, content } = await parsePostDetail(category, title)
 
-  // const parsingData = data as Post
+  console.log(data, content)
+  const parsingData = data as Post
 
-  // const readingMinute = Math.ceil(readingTime(content).minutes)
+  const readingMinute = Math.ceil(readingTime(content).minutes)
 
+  console.log(readingMinute)
   return (
     <Box margin={['0 0.5rem', '0 1.5rem', '0 1.5rem']}>
-      {/* <PostDetailHeader
+      <PostDetailHeader
         title={parsingData.title}
         date={parsingData.date}
         category={parsingData.category}
         readingMinute={readingMinute}
-      /> */}
+      />
       <Center height={'1rem'}>
         <Divider borderColor={'#F4F3EB'} />
       </Center>
@@ -30,7 +36,7 @@ const page = async ({ params: { category, title } }: PageParams) => {
       <Center height={'1rem'}>
         <Divider borderColor={'#F4F3EB'} />
       </Center>
-      {/* <PostComment /> */}
+      <PostComment />
     </Box>
   )
 }
