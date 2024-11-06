be7b4ffcf8e65bf0ad35d4e236a7292248ce672c
chhw130 <527coco@naver.com>
2024-11-06 19:33:01 +0900

    feat : update metadata

--- const/const.ts
+++ const/const.ts
@@ -1,15 +1,20 @@
 import type { Metadata } from 'next'
 
 const META_DATA = {
-  title: 'Mincho-Log',
+  title: '민초로그',
   url: 'https://www.mincho130.xyz/',
   description:
     '안녕하세요! 같이 고민하는 모험을 좋아하는 개발자, 최현우입니다.',
+  publisher: 'Mincho',
+  applicationName: 'Mincho-log',
 }
 
 export const defaultMeatdata: Metadata = {
   metadataBase: new URL(META_DATA.url),
-  title: META_DATA.title,
+  title: {
+    template: `${META_DATA.title} | %s`,
+    default: META_DATA.title,
+  },
   description: META_DATA.description,
   keywords: ['mincho', '527coco', 'chhw130', '민초', '기록'],
   openGraph: {
@@ -19,6 +24,8 @@ export const defaultMeatdata: Metadata = {
     images: '/images/mincho.png',
     type: 'website',
   },
+  publisher: META_DATA.publisher,
+  applicationName: META_DATA.applicationName,
 
   icons: {
     icon: '/images/mincho-rabbit.png',
