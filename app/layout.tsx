c75fb2a4ed150da7c7de837249d1c8654b884e45
chhw130 <527coco@naver.com>
2024-11-06 20:16:42 +0900

    fix : head position

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Gtag />
       <body>
-        <Gtag />
         <ChakraUIProvider>
           <ColorModeScript initialColorMode="system" />
           <Layout>{children}</Layout>
