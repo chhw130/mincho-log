9d91b1085aa0d28c864bbd959dfa247bc6e59543
chhw130 <527coco@naver.com>
2024-11-14 11:57:11 +0900

    chore : 주석 제거

--- component/layout/Layout.tsx
+++ component/layout/Layout.tsx
@@ -9,9 +9,9 @@ const Layout = ({ children }: PropsWithChildren) => {
   return (
     <Flex flexDir={'column'} h={'100%'} minH={'100vh'}>
       <Header />
-      {/* <Box w={'100%'} maxW={'968px'} margin={'0 auto'} flex={'1 1 0%'}>
+      <Box w={'100%'} maxW={'968px'} margin={'0 auto'} flex={'1 1 0%'}>
         {children}
-      </Box> */}
+      </Box>
       <Footer />
     </Flex>
   )
