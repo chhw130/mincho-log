9505ff1c8917e287c7a88e6c19ebf7a8dc665234
chhw130 <527coco@naver.com>
2024-12-17 12:01:43 +0900

    chore : 코드 정리

--- component/input/SerachBar.tsx
+++ component/input/SerachBar.tsx
@@ -1,32 +1,30 @@
 'use client'
 
 import { Post } from '@/types/post'
-import { throttle } from '@/util/\bthrottling'
-import { searchPost } from '@/util/post'
 import { Box, Button, Flex, Input, Text } from '@chakra-ui/react'
-import { PropsWithChildren, useCallback } from 'react'
+import { PropsWithChildren } from 'react'
 
 interface SearchBarProps extends PropsWithChildren {
-  setPostList: any
+  setPostList: () => void
   postList: Post[]
 }
 
-const SerachBar = ({ setPostList, postList }: SearchBarProps) => {
-  const handleSearchPostChange = (keyword: string) => {
-    setPostList(() => {
-      return searchPost(keyword, postList)
-    })
-  }
+const SerachBar = ({}: SearchBarProps) => {
+  // const handleSearchPostChange = (keyword: string) => {
+  //   setPostList(() => {
+  //     return searchPost(keyword, postList)
+  //   })
+  // }
 
   // const memorizedHandleSearchPostChange = useCallback(
   //   (keyword: string) => throttle(handleSearchPostChange(keyword), 10000),
   //   [handleSearchPostChange],
   // )
   // 쓰로틀링 적용
-  const throttledSearch = useCallback(
-    throttle(handleSearchPostChange, 1000), // 1초 간격으로 쓰로틀링
-    [handleSearchPostChange],
-  )
+  // const throttledSearch = useCallback(
+  //   throttle(handleSearchPostChange, 1000), // 1초 간격으로 쓰로틀링
+  //   [handleSearchPostChange],
+  // )
   return (
     <Box
       as="form"
@@ -39,7 +37,7 @@ const SerachBar = ({ setPostList, postList }: SearchBarProps) => {
       <Text padding={'0.3rem 0'}>포스트를 검색해보세요.</Text>
       <Flex>
         <Input
-          onChange={({ target }) => throttledSearch(target.value)}
+          // onChange={({ target }) => throttledSearch(target.value)}
           placeholder="글의 제목이나 설명에 관련된 키워드를 검색하세요."
         />
         <Button type="submit" />
